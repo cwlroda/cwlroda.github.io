181aa4c61ca173db949ec81eff91ff8a0c247511
cwlroda <weiloon.c97@gmail.com>
2021-06-20 02:21:48 +0800

    Added analytics

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,7 +4,7 @@
 const settings = {
   isSplash: true, // Change this to true if you want to use the splash screen.
   useCustomCursor: false, // Change this to false if you want the good'ol cursor
-  googleTrackingID: "",
+  googleTrackingID: "G-SDN08JYNZG",
 };
 
 //Home Page
