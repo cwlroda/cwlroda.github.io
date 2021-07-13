ddfe67716326c8853395979476d6d04a6fa5bae8
cwlroda <weiloon.c97@gmail.com>
2021-07-13 21:21:39 +0800

    Separated lotties for each page

--- src/components/DisplayLottie.jsx
+++ src/components/DisplayLottie.jsx
@@ -1,6 +1,31 @@
 import React, { Suspense } from 'react';
 import Lottie from 'react-lottie';
 
+const EducationLottie = ({ name, animationData }) => {
+  var defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      width: '100%',
+      height: '100%',
+      preserveAspectRatio: 'xMaxYMax meet',
+      viewBoxSize: '0 100 506.06371 500.2279'
+    },
+  }
+
+  return (
+    <Suspense>
+      {/* To override default onClick pause by Lottie */}
+      <div onClick={() => null}>
+        <Lottie
+          options={defaultOptions}
+        />
+      </div>
+    </Suspense>
+  )
+}
+
 const SkillsLottie = ({ name, animationData }) => {
   var defaultOptions;
 
@@ -80,4 +105,27 @@ const SkillsLottie = ({ name, animationData }) => {
   )
 }
 
-export default SkillsLottie;
+const ContactLottie = ({ name, animationData }) => {
+  var defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+  }
+
+  return (
+    <Suspense>
+      {/* To override default onClick pause by Lottie */}
+      <div onClick={() => null}>
+        <Lottie
+          options={defaultOptions}
+        />
+      </div>
+    </Suspense>
+  )
+}
+
+export {
+  EducationLottie,
+  SkillsLottie,
+  ContactLottie,
+};
